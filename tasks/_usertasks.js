49f96d3c4241eeaaf37d5a119c50f99df90afc3d
Matt Karl <matt@cloudkid.com>
2014-08-20 15:35:43 -0400

    Added lodash dependency

--- tasks/_usertasks.js
+++ tasks/_usertasks.js
@@ -2,14 +2,15 @@
 
 module.exports = function(grunt)
 {
+	var _ = require('lodash');
+
 	grunt.registerTask('_usertasks', function(){
-		var _ = grunt.util._,
-			allTasks = {},
-			// Grab only the local user tasks
+		var allTasks = {},
 			userTasks = _.filter(grunt.task._tasks, function(task){
+				// Grab only the local user tasks
 				return !task.multi && !/local Npm module/.test(task.meta.info);
 			});
-
+			
 		// Format the tasks into an dictionary
 		_.each(userTasks, function(task){
 			allTasks[task.name] = task.info;
